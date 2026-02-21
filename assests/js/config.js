1f149706d583ad2356ff7153e9de815c16963aae
Julio Costa Braga da Silva <julio.silva@anagaming.com.br>
2026-02-21 15:42:02 -0300

    Update SLA_ENDPOINT and increase REFRESH_MS

--- assests/js/config.js
+++ assests/js/config.js
@@ -1,10 +1,10 @@
 window.APP_CONFIG = {
   // Apps Script (Painel Geral)
-  SLA_ENDPOINT: "https://script.google.com/macros/s/AKfycbzsFtphjBe__IrZv5gzqMDT8qEmkXi3rHb62W_rzaULSeqsigye2BXUVNGKT91Dc8oi/exec",
+  SLA_ENDPOINT: "https://script.google.com/macros/s/AKfycbwT4HZPjvzC0ReOd6wpC0xhqoAnUbLU2ntj3ir5SVQ7MUFuvsERrq_sIUAbONFhwbLR/exec",
 
   // FastAPI (Mongo -> Sheets) - vazio = mesma origem (quando servir via FastAPI)
   SYNC_API_BASE: "",
 
   // Atualização automática do Painel Geral
-  REFRESH_MS: 300000
+  REFRESH_MS: 600000
 };
