417f6eb37431dc2dd35a665c18f3dd2d3bb02b89
Julio Costa Braga da Silva <julio.silva@anagaming.com.br>
2026-02-21 15:49:15 -0300

    Update SLA_ENDPOINT URL in config.js

--- assests/js/config.js
+++ assests/js/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   // Apps Script (Painel Geral)
-  SLA_ENDPOINT: "https://script.google.com/macros/s/AKfycbzsFtphjBe__IrZv5gzqMDT8qEmkXi3rHb62W_rzaULSeqsigye2BXUVNGKT91Dc8oi/exec",
+  SLA_ENDPOINT: "https://script.google.com/macros/s/AKfycbxE4gr69DwPvrvW6T5JOyi6EruWnniFVTCiqPYvMQfC4r-CnnuCfJez7u4XE5UufWJD/exec",
 
   // FastAPI (Mongo -> Sheets) - vazio = mesma origem (quando servir via FastAPI)
   SYNC_API_BASE: "",
